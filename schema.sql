6794f72b1fad231102d681dff00c7badcd9bc6f7
ilanalevsh <ilanalevsickaa@gmail.com>
2026-05-14 01:07:54 +0300

    Create tables for users, services, and requests

--- schema.sql
+++ schema.sql
@@ -1,1 +1,18 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    username TEXT,
+    email TEXT,
+    password TEXT
+);
 
+CREATE TABLE services (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    description TEXT
+);
+
+CREATE TABLE requests (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    service_id INTEGER
+);
